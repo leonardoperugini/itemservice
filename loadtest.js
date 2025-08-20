ff340e4e002e1ca64efba05af174a425ba7c8631
Leonardo Perugini <leonardo.perugini2@studio.unibo.it>
2025-08-20 17:42:14 +0200

    migrating from gatling to k6

---
+++ loadtest.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  vus: 10, // utenti virtuali
+  duration: '30s',
+};
+
+export default function () {
+  const res = http.get('http://localhost:8080/api/items'); // Modifica endpoint se necessario
+  check(res, { 'status was 200': (r) => r.status === 200 });
+  sleep(1);
+}
